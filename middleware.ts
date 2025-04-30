59920222f0018157b98bdc72095b425007cb976f
rafael r. camargo <66796237+rafaelrcamargo@users.noreply.github.com>
2025-04-30 16:47:02 -0300

    feat: enhance logging for proxy headers and URLs in middleware

--- middleware.ts
+++ middleware.ts
@@ -3,22 +3,28 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((_, req) => {
   if (req.nextUrl.pathname.match("__clerk")) {
-    console.log("Path", req.nextUrl.pathname)
+    console.log("Path", req.nextUrl.pathname);
 
     const proxyHeaders = new Headers(req.headers);
     proxyHeaders.set(
       "Clerk-Proxy-Url",
-      process.env.NEXT_PUBLIC_CLERK_PROXY_URL || "",
+      process.env.NEXT_PUBLIC_CLERK_PROXY_URL || ""
     );
+    console.log(
+      "NEXT_PUBLIC_CLERK_PROXY_URL",
+      process.env.NEXT_PUBLIC_CLERK_PROXY_URL
+    );
+
     proxyHeaders.set("Clerk-Secret-Key", process.env.CLERK_SECRET_KEY || "");
-    if ((req as any)?.ip) {
-      proxyHeaders.set("X-Forwarded-For", (req as any).ip);
-    } else {
-      proxyHeaders.set(
-        "X-Forwarded-For",
-        req.headers.get("X-Forwarded-For") || "",
-      );
-    }
+    console.log("CLERK_SECRET_KEY", process.env.CLERK_SECRET_KEY);
+
+    proxyHeaders.set(
+      "X-Forwarded-For",
+      req.headers.get("X-Forwarded-For") || ""
+    );
+    console.log("X-Forwarded-For", req.headers.get("X-Forwarded-For"));
+
+    proxyHeaders.forEach((value, key) => console.log("Header", key, value));
 
     const proxyUrl = new URL(req.url);
     proxyUrl.host = "frontend-api.clerk.dev";
@@ -26,12 +32,10 @@ export default clerkMiddleware((_, req) => {
     proxyUrl.protocol = "https";
     proxyUrl.pathname = proxyUrl.pathname.replace("/__clerk", "");
 
-    console.log("Proxy URL", proxyUrl.toString())
+    console.log("Proxy URL", proxyUrl.toString());
 
     return NextResponse.rewrite(proxyUrl, {
-      request: {
-        headers: proxyHeaders,
-      },
+      request: { headers: proxyHeaders }
     });
   }
 });
@@ -41,6 +45,6 @@ export const config = {
     // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes AND anything passed through the proxy
-    "/(api|trpc|__clerk)(.*)",
-  ],
+    "/(api|trpc|__clerk)(.*)"
+  ]
 };
